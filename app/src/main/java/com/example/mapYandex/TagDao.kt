bb16b1643ceba4e6f7efd9115138f00fcace13b3
Андрей Ковлев (SeReRaGuy) <sereraguy654@gmail.com>
2024-01-05 14:30:31 +0300

    Наделал кучу неработающего кода, решил пока что по кнопке открывать список (интересно, ошибка ли то, что карта - не фрагмент?)

--- app/src/main/java/com/example/mapYandex/TagDao.kt
+++ app/src/main/java/com/example/mapYandex/TagDao.kt
@@ -1,29 +1,29 @@
-package com.example.mapYandex
-
-import androidx.lifecycle.LiveData
-import androidx.room.Dao
-import androidx.room.Delete
-import androidx.room.Insert
-import androidx.room.Query
-import androidx.room.Update
-
-@Dao
-interface TagDao {
-    @Insert
-    suspend fun insert(vararg Tags: Tag)
-
-    @Insert
-    suspend fun insert(Tags: Tag)
-
-    @Query("SELECT * FROM tag")
-    fun findAll(): LiveData<List<Tag>>
-
-    @Query("SELECT * FROM tag WHERE id=:id LIMIT 1")
-    fun findById(id: Int): LiveData<Tag>
-
-    @Update
-    suspend fun update(tag: Tag): Int
-
-    @Delete
-    suspend fun delete(tag: Tag): Int
-}
+//package com.example.mapYandex
+//
+//import androidx.lifecycle.LiveData
+//import androidx.room.Dao
+//import androidx.room.Delete
+//import androidx.room.Insert
+//import androidx.room.Query
+//import androidx.room.Update
+//
+//@Dao
+//interface TagDao {
+//    @Insert
+//    suspend fun insert(vararg Tags: Tag)
+//
+//    @Insert
+//    suspend fun insert(Tags: Tag)
+//
+//    @Query("SELECT * FROM tag")
+//    fun findAll(): LiveData<List<Tag>>
+//
+//    @Query("SELECT * FROM tag WHERE id=:id LIMIT 1")
+//    fun findById(id: Int): LiveData<Tag>
+//
+//    @Update
+//    suspend fun update(tag: Tag): Int
+//
+//    @Delete
+//    suspend fun delete(tag: Tag): Int
+//}
