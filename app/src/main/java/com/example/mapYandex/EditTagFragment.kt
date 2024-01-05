449c8f8184890dd80c63ff8941ed0b5a29f068ac
Андрей Ковлев (SeReRaGuy) <sereraguy654@gmail.com>
2024-01-05 09:32:18 +0300

    Накидал разметочку, есть чуть чуть классов

---
+++ app/src/main/java/com/example/mapYandex/EditTagFragment.kt
@@ -0,0 +1,13 @@
+package com.example.mapYandex
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class EditTagFragment {
+    private var _binding:
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+
+    }
+}
---
+++ app/src/main/java/com/example/mapYandex/EditTagViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.mapYandex
+
+class EditTagViewModel {
+}
